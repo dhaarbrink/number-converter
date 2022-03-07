23d0e11a45237ffe93fc1e6a2d8086870510be7a
Dennis Haarbrink <dennis@nonmagna.nl>
2022-03-07 10:17:31 +0100

    Made Token a data class

--- src/main/kotlin/converter/token/Token.kt
+++ src/main/kotlin/converter/token/Token.kt
@@ -1,7 +1,3 @@
 package converter.token
 
-class Token(val value: Long, val type: Type, val commit: Boolean = false) {
-    override fun toString(): String {
-        return "[value: $value, type: $type, commit: $commit]"
-    }
-}
+data class Token(val value: Long, val type: Type, val commit: Boolean = false)
